57ddec5352b9c20e1a3e786e7ba459f4052be99e
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-04 16:15:37 +0900

    실습1 - 1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    
+    for(i = 0; i < 10; i++)
+        printf("*");
+    for(i = 0; i < 10; i++)
+        printf("*");
+    for(i = 0; i < 10; i++)
+        printf("*");
+  
+  system("PAUSE");	
+  return 0;
+}
