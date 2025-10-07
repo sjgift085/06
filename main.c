d94100fb4be53087774aaf31342ab58665da3c06
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-07 17:24:49 +0900

    실습 03

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func(void)
+int sumTwo(int a, int b)
 {
-     int x;
-     printf("func x is at %p\n", &x);
+    return(a+b);
+}
+
+int square(int n)
+{
+    return(n*n);
+}
+
+int get_max(int a, int b)
+{
+    if(a > b)
+        return a;
+    else
+        return b;
 }
 
 int main(void)
 {
-    int x;
-    printf("main x is at %p\n", &x);
-    func();
+    printf("sum two result is %i\n", sumTwo(2,3));
+    printf("square result is %i\n", square(5));
+    printf("getmax result is %i\n", get_max(10, 13));
     
   system("PAUSE");	
   return 0;
