f1187f3208eea9589e0c753322450e64b52679b0
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-07 17:42:09 +0900

    실습 5

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a)
+int factorial(int n)
 {
-     return(a*a);
+    int i;
+    int res = 1;
+    
+    for(i = 1; i <= n; i++)
+          res = res * i;
+          
+    return res;
+}
+
+int combination(int n, int r)
+{
+     return(factorial(n)/factorial(n-r)/factorial(r));
 }
 
 int main(void)
 {
-    int a = 2;
-    a = square(a);
-    printf("a=%i\n", a);
+    int n, r;
+    int res;
+    
+    printf("n과 r을 입력하시오: ");
+    scanf("%i %i", &n, &r);
+    
+    res = combination(n, r);
+    
+    printf("combination result is : %i\n", res);
     
   system("PAUSE");	
   return 0;
